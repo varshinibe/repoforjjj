e02e7976a22d919c3d53cf4016adba649986e849
Dell <Dell@DESKTOP-BE5FK59>
2022-09-30 15:28:43 +0530

    jenkins commit

---
+++ src/test/java/jenkinspracticepackage/Jenkinstest.java
@@ -0,0 +1,16 @@
+package jenkinspracticepackage;
+
+import org.testng.annotations.Test;
+
+public class Jenkinstest {
+	
+	@Test
+	public void create()
+	{
+		System.out.println("----created jenkins---");
+	}
+	
+	
+	
+
+}
